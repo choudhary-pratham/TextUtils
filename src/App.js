42228986d388b668d3d4151bd4a0fe3984434596
Pratham Choudhary <prathamchoudhary23@gmail.com>
2023-04-20 18:19:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Navbar from "./components/Navbar";
-import TextForm from "./components/Textformat";
+import Textformat from "./components/Textformat";
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
-    <TextForm/>
+    <Textformat/>
     </>
   );
 }
--- src/components/Textformat.js
+++ src/components/Textformat.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Textformat() {
+
+  const [textVal,setText] = useState('');
+
+  const handleonChange = (event)=>{
+    setText(event.target.value);
+  }
+  const handleUpClick = ()=>{
+    setText(textVal.toUpperCase());
+  }
+  const handleLoClick = ()=>{
+    setText(textVal.toLowerCase());
+  }
+  const handleClsClick = ()=>{
+    setText('');
+  }
+
+  const handleCapClick = ()=>{
+    if(textVal !== ''){
+      let newString = "";
+      let words = textVal.split(' ');
+      for(let i = 0;i<words.length;i++){
+        newString += words[i][0].toUpperCase()+words[i].slice(1).toLowerCase();
+        if(i!==words.length-1) newString += ' ';
+      }
+      setText(newString);
+    }
+  }
+
   return (
     <div className='flex flex-col'>
-      <textarea className = "border border-black p-2 flex item-center m-auto mt-2" placeholder= "Enter your text here"cols="120" rows="8"></textarea>
+      <textarea className = "border border-black p-2 flex item-center m-auto mt-2" value={textVal} onChange={handleonChange} placeholder= "Enter your text here"cols="120" rows="15"></textarea>
       <div className="btn flex m-auto">
-        <button className='p-2 m-2 bg-red-500 text-white rounded-md hover:bg-red-300 hover:text-black'>Convert to upper case</button>
-        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black'>Convert to lower case</button>
-        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black'>Convert to Capitalize case</button>
-        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black'>Convert to title case</button>
-        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black'>Clear</button>
+        <button className='p-2 m-2 bg-red-500 text-white rounded-md hover:bg-red-300 hover:text-black' onClick={handleUpClick}>Convert to upper case</button>
+        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black' onClick={handleLoClick}>Convert to lower case</button>
+        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black' onClick={handleCapClick}>Convert to Capitalize case</button>
+        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black'>Convert to alternating case</button>
+        <button className='p-2 m-2 bg-red-500 text-white rounded-md  hover:bg-red-300 hover:text-black' onClick={handleClsClick}>Clear</button>
       </div>
     </div>
   )
