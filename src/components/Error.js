e3ac298f648a34252804688ceb5a84e3868c3352
Pratham Choudhary <prathamchoudhary23@gmail.com>
2023-04-20 19:31:21 +0530

    Modified Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error() {
   return (
     <div>
-        <img src="https://www.portent.com/images/2020/07/404-Error-Image.jpg" alt="Error404" className='h-[650px] w-[900px] flex justify-center items-center m-auto'/>
+        <img src="https://www.portent.com/images/2020/07/404-Error-Image.jpg" alt="Error404" className='sm:h-[350px] sm:w-[400px] h-[650px] w-[900px] flex justify-center items-center m-auto'/>
     </div>
   )
 }
